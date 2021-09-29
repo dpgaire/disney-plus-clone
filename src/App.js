295e679bae5c217853ccf80bae9414ac04dee3b7
dpgaire <gairhedurga13@gmail.com>
2021-09-29 16:17:22 +0545

    Login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';  
 import Details from './components/Details';
+import Login from './components/Login';
 import { 
   BrowserRouter as Router, 
   Switch,
@@ -15,6 +16,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+              <Login />
+          </Route>
           <Route path="/details">
             <Details />
           </Route>
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Login() {
+    return (
+        <Container>
+            Login Page
+        </Container>
+    )
+}
+
+export default Login
+
+const Container =styled.div `
+`
